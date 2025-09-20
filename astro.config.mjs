5409da0736af388c6a6b7b37adaf0357b8f321cf
Moemu <WhitemuTeam@outlook.com>
2025-09-20 17:50:47 +0800

    🚚 更新博客地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://preview.snowy.moe/",
+	site: "https://blog.snowy.moe/",
 
 	base: "/",
 	trailingSlash: "always",
